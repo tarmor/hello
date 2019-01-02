10d6977c2ce63c0aa60160cb17c94d0f292bb6a4
Egle Puppart <egle.puppart@gmail.com>
2019-01-02 16:44:24 +0200

    tehtud

---
+++ yl11.py
@@ -0,0 +1,7 @@
+kasutaja_loom = str (input ("Sisesta oma lemmik loom: ")) 
+print (str(kasutaja_loom))
+
+loomad = ["koer", "kass", "kana", "merisiga"]
+loomad.append(kasutaja_loom)
+print (loomad)
+print (loomad [-1][-1])
