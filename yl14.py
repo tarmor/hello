215f30e5717978ec2cb52e0800abdd8406178fa9
Egle Puppart <egle.puppart@gmail.com>
2019-01-11 13:36:00 +0200

    tehtud

---
+++ yl14.py
@@ -0,0 +1,9 @@
+text = "Mu Isamaa mu õnn ja rööm...."
+th = ["a", "e", "i", "o", "u", "õ", "ä", "ö", "ü"]
+count = 0
+for item in text:
+    if item in th:
+        count = count + 1
+        print (item)
+print ("Tekstis on " + str (count) + " täishäälikut.") 
+
